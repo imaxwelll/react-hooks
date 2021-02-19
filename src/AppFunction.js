2eb22e31e7f53b0c0a5fa419b90de865c68550ba
Maksym Bilashenko <mbilashenko@conceptsoft.io>
2021-02-19 13:18:01 +0200

    first commit

---
+++ src/AppFunction.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [isOn, setIsOn] =  useState(false);
+  const [mousePosition, setMousePosition] = useState({ x: null, y: null });
+  const [status, setStatus] = useState(navigator.onLine)
+
+  useEffect(
+    () => {
+      document.title = `You have clicked ${count} times`;
+      window.addEventListener('mousemove', handleMouseMove)
+      window.addEventListener('online', handleOnline)
+      window.addEventListener('offline', handleOffline)
+
+      return () => {
+        window.removeEventListener('mousemove', handleMouseMove);
+        window.removeEventListener('online', handleOnline);
+      window.removeEventListener('offline', handleOffline);
+      }
+    }, [count])
+
+    const handleOnline = () => {
+      setStatus(true)
+    }
+
+    const handleOffline = () => {
+      setStatus(false)
+    }
+
+    const handleMouseMove = event => {
+      setMousePosition({
+        x: event.pageX,
+        y: event.pageY
+      })
+  }
+
+  const incrementCount = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const toggleLight = () => {
+    setIsOn(prevIsOn => !prevIsOn)
+  }
+
+  return (
+    <>
+      <h2>Counter</h2>
+      <button onClick={incrementCount}>I was clicked {count} times</button>
+
+      <h2>Toggle Light</h2>
+      <div
+        style={{
+          height: "50px",
+          width: "50px",
+          background: isOn ? "yellow" : "grey",
+        }}
+        onClick={toggleLight}
+      ></div>
+
+      <h2>Mouse Position</h2>
+      {JSON.stringify(mousePosition, null, 2)}
+      <br />
+
+      <h2>Network Status</h2>
+      <p>You are <strong>{status ? "online" : "offline"}</strong></p>
+    </>
+  );
+};
+
+export default App;
